b7065a1cacf970eb4e88f6a6cf9b69e261246680
Amaury Padilla <amaurypadilla@Amaurys-MacBook-Pro.local>
2026-08-17 17:36:10 -0600

    add fix in allowed host

--- core/settings.py
+++ core/settings.py
@@ -23,10 +23,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = env.list(
-    "ALLOWED_HOSTS",
-    default=['localhost', '127.0.0.1', '10.0.2.2', 'hfiles-dev.cloudteam.net'],
-)
+ALLOWED_HOSTS =['hfiles-dev.cloudteam.net'],
 
 # --- Autenticación (frontend fuera del Admin) ---
 LOGIN_URL = 'authentication:login'
